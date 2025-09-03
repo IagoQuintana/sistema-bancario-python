0d6fa3d6d9be1fd883c05f498430b3781d04b55f
Iago Quintana Gonçalves <152810725+IagoQuintana@users.noreply.github.com>
2025-09-03 16:14:55 -0300

    Add files via upload
    versão final desafio sistema bancário DIO

--- desafioV2.py
+++ desafioV2.py
@@ -1,87 +1,137 @@
+import sys
 
-def depositar(saldo,extrato):
-    print("------------- DEPÓSITO -------------")
-    deposito = float(input("Informe o valor do depósito!\n"))
 
-    while deposito <= 0:
-      print("Valor informado está fora do esperado, o depósito deve ser um valor positivo.")
-      deposito = float(input("Informe o valor do depósito!\n"))
-        
-    saldo += deposito 
-    print("Depósito realizado!")
-    extrato += f'Depósito:\tR$ {deposito:.2f}\n'
-    return saldo,extrato
+def conferir_cadastro(usuarios):
+    cpf = input("Informe o CPF do usuário (somentos números):\n ")
+    usuario = [usuario for usuario in usuarios if usuario['cpf'] == cpf]
+    return cpf, usuario[0] if usuario else None
 
-def sacar(saldo,extrato,numero_saques,limite_saques):
-    print("------------- SAQUE -------------")
-    saque = float(input("Informe o valor do saque!\n"))
 
-    while saque > 500 or saque <=0 or saque > saldo:
-        if saque > 500:
-            print("O limite de saque é R$500,00")
-        elif saque <= 0:
-            print("O valor de saque deve ser maior que R$0,00")
-        elif saque > saldo:
-            print("Sem saldo suficiente para o saque!")
+def cadastrar_usuario(usuarios):
+    cpf, usuario = conferir_cadastro(usuarios)
+    if usuario:
+        print("Usuário já cadastrado\n")
+        return
+    nome = input("Informe o nome para cadastro: ")
+    data_nascimento = input("Informe da data de nascimento: ")
+    endereco = input("Informe o endereço: ")
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento,
+                    "cpf": cpf, "endereço": endereco})
+    print("Usuário cadastrado!")
 
-        saque = float(input("Informe o valor do saque!\n"))
-    if numero_saques >= limite_saques:
-        print("Limite diário de saques atingido!\n")
-    else:           
-        numero_saques += 1
-        saldo -= saque
-        print("Saque realizado!")
-        extrato += f'Saque:\t\tR$ {saque:.2f}\n'
-    return saldo,extrato,numero_saques
 
-def tirar_extrato(saldo,extrato):
-    print("------------- EXTRATO -------------")
-    print(extrato)
-    print(f'\nSaldo: R$ {saldo:.2f}')
-    
-    return saldo, extrato
+def abrir_conta(AGENCIA, numero_conta, usuarios):
+    cpf, usuario = conferir_cadastro(usuarios)
+    if usuario:
+        conta = {'agencia': AGENCIA, 'c/c': numero_conta,
+                 'titular': usuario, 'saldo': 0, "extrato": ""}
+        print("Conta aberta com sucesso!\n")
+        return conta
+    print("Usúario não cadastrado! Abertura de conta encerrada.\n ")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        print(
+            f"Agência: {conta['agencia']}\ncc: {conta['c/c']}\nTitular: {conta['titular']['nome']}\nSaldo: {conta['saldo']}\n")
+
+
+def depositar(contas, usuarios):
+    cpf, usuario = conferir_cadastro(usuarios)
+    if not usuario:
+        print("Usuário não encontrado")
+        return
+    cc = int(input("Informe o número da conta corrente"))
+    for conta in contas:
+        if conta['c/c'] == cc and usuario['nome'] == conta['titular']['nome']:
+            deposito = int(input("Informe o valor do deposito: "))
+            conta['saldo'] += deposito
+            conta['extrato'] += f"Depósito:+R${deposito}\n "
+            break
+
+    else:
+        print("Conta não encontrada")
+
+
+def sacar(contas, usuarios):
+    cpf, usuario = conferir_cadastro(usuarios)
+    if not usuario:
+        print("Usuário não encontrado")
+        return
+    cc = int(input("Informe o número da conta corrente"))
+
+    for conta in contas:
+        if conta['c/c'] == cc and usuario['nome'] == conta['titular']['nome']:
+            saque = int(input("Informe o valor de saque"))
+            while saque > conta['saldo']:
+                saque = int(
+                    input("Saldo insuficiente, informe novo valor de saque! "))
+            conta['saldo'] -= saque
+            conta['extrato'] += f"Saque:-R${saque}\n "
+            break
+    else:
+        print("Conta não encontrada")
+
+
+def exibir_extrato(contas, usuarios):
+    cpf, usuario = conferir_cadastro(usuarios)
+    if not usuario:
+        print("Usuário não encontrado")
+        return
+    cc = int(input("Informe o número da conta corrente"))
+    for conta in contas:
+        if conta['c/c'] == cc and usuario['nome'] == conta['titular']['nome']:
+            print(f"Extrato: ")
+            print(
+                f"Agência: {conta['agencia']}\nConta: {conta['c/c']}\nTitular: {conta['titular']['nome']}\n{conta['extrato']}")
+            break
+    else:
+        print("Conta não encontrada")
+
 
 def main():
-    
     menu = """
+        [nu] Novo usuário
+        [nc] Nova conta
+        [lc] Listar contas
         [d] Depositar
         [s] Sacar
         [e] Extrato
         [q] Sair
-            
-            """
-            
-    saldo = 0.00
-    limite = 500.00
-    extrato = ""
-    numero_saques = 0
-    limite_saques = 3
-    
+"""
+
+    LIMITE_SAQUEs = 3
+    AGENCIA = "001"
+    contas = []
+    usuarios = []
     while True:
+        print(menu)
+        entrada = input("Escolha a opção desejada: ")
 
-        opcao = input(menu)
+        if entrada == 'nu':
+            cadastrar_usuario(usuarios)
 
+        elif entrada == 'nc':
+            numero_conta = len(contas) + 1
+            conta = abrir_conta(AGENCIA, numero_conta, usuarios)
+            if conta:
+                contas.append(conta)
 
-        if opcao == 'd':
-            saldo,extrato = depositar(saldo,extrato)
-                            
-        
-        elif opcao == 's':
-            saldo,extrato,numero_saques = sacar(saldo,extrato,numero_saques,limite_saques)
-          
-        
-        elif opcao == 'e':
-            saldo,extrato = tirar_extrato(saldo,extrato)
+        elif entrada == 'lc':
+            listar_contas(contas)
 
-    
-        elif opcao =='q':
-            print("Sistema encerrado!")
-            break;
-    
-        else:
-            print("Operação inválida, por favor selecione novamente a operação desejada.")
+        elif entrada == 'd':
+            depositar(contas, usuarios)
+
+        elif entrada == 's':
+            sacar(contas, usuarios)
+
+        elif entrada == 'e':
+            exibir_extrato(contas, usuarios)
+
+        elif entrada == 'q':
+            break
 
 
 if __name__ == '__main__':
     main()
-
